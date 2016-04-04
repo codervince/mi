d293c96b060503c68a2b17f332d8379243ab3681
fonar <mishnya@mail.ru>
2016-04-04 14:52:45 +0300

    Tests for System permissions

--- systems/tests.py
+++ systems/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test                import TestCase
+from django.contrib.auth.models import User
+from guardian.shortcuts         import assign_perm
+from guardian.shortcuts         import remove_perm
+from systems.models             import System
 
-# Create your tests here.
+class SystemPermissionsTestCase( TestCase ):
+
+    def test_system_permissions( self ):
+
+        # Create new User and System
+        system = System.objects.create( systemname = 'test_system', snapshotid = 0, isActive = True, isTurf = True, exposure = ()  )
+        user   =   User.objects.create( username   = 'test_user' )
+
+        # Check that User don't have permission for System initially
+        self.assertFalse( user.has_perm( 'view_system', system ) )
+
+        # Set permissions on User for System
+        assign_perm( 'view_system', user, system )
+
+        # Check that User have permission for System
+        self.assertTrue( user.has_perm( 'view_system', system ) )
+
+        # Set permissions on User for System
+        remove_perm( 'view_system', user, system )
+
+        # Check that User don't have permission for System again
+        self.assertFalse( user.has_perm( 'view_task', system ) )
