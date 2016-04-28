02aeb4a36c56f21a3030c4844ac1af025145974c
codervince <publicvince102@gmail.com>
2016-04-28 04:30:00 +0200

    work in sub import; functional tests redux

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,10 +4,236 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from django.test import Client, TestCase
+from selenium.webdriver.common.by import By
+from contextlib import contextmanager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import staleness_of
+
 import unittest
 
 USER_NAME = 'tester1'
 PASSWORD = 'pa$$w0rd'
+
+USER = {
+    'username': 'Tester2',
+    'firstname': 'TESTER2',
+    'email': "wahwah@example.com",
+    'password': "pa$$w0rd"
+
+}
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    @contextmanager
+    def wait_for_page_load(self, timeout=30):
+        old_page = self.browser.find_element_by_tag_name('html')
+        yield
+        WebDriverWait(self.browser, timeout).until(
+            staleness_of(old_page)
+        )
+
+
+
+
+    # test offline then switch URL
+    def test_can_see_subscription_and_start_up(self):
+
+        # Abbot has read a tweet about a site and follow the link to the home page.
+        self.browser.get('http://localhost:8000/')
+
+        #he expects to see the welcome page..
+        self.assertIn('Metainvest.io', self.browser.title)
+
+        # He expected to see some text about the site
+        # explanatory_text = "Statistical methods of handicapping"
+        body_text = self.browser.find_element_by_tag_name("body").text
+        self.assertIn("Statistical methods of handicapping", body_text)
+
+        # ...somewhere to read about how much it costs
+        # Look for subscription panel
+        subscription_info = self.browser.find_element_by_id("subscriptions")
+        self.assertIn( 'Subscription Plans', subscription_info.text)
+
+        # ...and a link to sign up
+        # /account/registration/
+
+        # He decides to sign up and is taken to a form
+        # self.browser.find_element(By.PARTIAL_LINK_TEXT("registration")).click()
+        self.browser.find_element_by_link_text("Sign up").click()
+
+
+        # We were taken to the sign up page? Look for a form with a repeat password input 2
+        # id_password2
+
+        input_element= self.browser.find_element(By.XPATH, "//input[@id='id_password2']")
+        self.assertIsNotNone(input_element)
+
+        # He fills out the form with Username, firstname password
+        signup_form = self.browser.find_element(By.TAG_NAME, "form")
+
+        self.assertIsNotNone(signup_form)
+
+
+        # first he fills out form and submits with no info. Expect validation error
+        self.browser.find_element_by_name("submit").click()
+        body_text = self.browser.find_element_by_tag_name("body").text
+        self.assertIn("field is required", body_text)
+
+        # Then he decides to do it properly
+
+        username = self.browser.find_element_by_id("id_username")
+        first_name = self.browser.find_element_by_id("id_first_name")
+        email = self.browser.find_element_by_id("id_email")
+        password = self.browser.find_element_by_id("id_password")
+        password2 = self.browser.find_element_by_id("id_password2")
+
+        username.send_keys(USER['username'])
+        first_name.send_keys(USER['firstname'])
+        email.send_keys(USER['email'])
+        password.send_keys(USER['password'])
+        password2.send_keys(USER['password'])
+        self.browser.find_element_by_name("submit").click()
+
+        body_text = self.browser.find_element_by_tag_name("body").text
+        self.assertNotIn("field is required", body_text)
+
+        ## how else do we know that the form has been submitted properly?
+
+        # After enter his details, he is signed in and taken to the systems/systems page
+
+        #reffresh page?
+        ##TODO: does not redirect!
+
+        # with self.wait_for_page_load(timeout=30):
+        #     ## what is on the systems/systems page?
+        #
+        #     self.assertEqual(self.browser.current_url, "http://localhost:8000/systems/systems")
+        #
+        #     h2_tags = self.browser.find_elements_by_tag_name('h2')
+        #
+        #     self.assertIn(
+        #         any(tag.text== 'Season Performance' for tag in h2_tags)
+        #     )
+
+
+    def test_nonsignedinusers_cannot_see_systems_systems(self):
+
+        # A visitor Val tries to go to systems/systems and is rediteced to the landing page
+
+        self.browser.get('http://localhost:8000/systems/systems')
+
+        self.assertNotEqual(self.browser.title, "Systems Page")
+
+    def test_user_login_and_systemssystems_page(self):
+
+        # User is logged in
+        ## Assume USER is created
+        # got to login page and sign in
+
+        self.browser.get("http://metainvest.io/account/login/")
+
+        username = self.browser.find_element_by_id("id_username")
+        password = self.browser.find_element_by_id("id_password")
+
+        username.send_keys(USER['username'])
+        password.send_keys(USER['password'])
+
+        #TODO: Why doesnt user log in?
+        self.browser.find_element_by_name("login").click()
+
+        # should now be on the right page
+        self.browser.get('http://localhost:8000/systems/systems')
+
+        print(self.browser.title)
+
+        logout_link_text = self.browser.find_element(By.PARTIAL_LINK_TEXT, "Logout")
+        self.assertisNotNone(logout_link_text)
+
+        # Billy should be logged in - look for a href /account/logout
+        logout_tag = self.browser.find_element_by_xpath("//a[@href='/account/logout']")
+        self.assertNotFalse(logout_tag)
+
+
+        # He can view systems color coded based on viability = 3 factors
+
+
+        self.assertEqual(self.browser.title, "Systems Page")
+
+        # He sees all the systems currently active on this page
+
+
+        # ALl the systems lead to the systems detail page
+
+
+        # He chooses one SYSTEM =  and clicks the link
+
+
+
+# def test_if_jquery_is_working(self):
+    #
+    #     element = self.browser.execute_script("return $('.row')[0]")
+
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
+
+
+
+
+
+
+
+# On this System detail page, he can see 2016 stats
+
+
+# On this page he can see 2015 stats including winsr, a-e and wins, runs and archie
+
+
+# He fits subscribe but has no money and is taken to a Pay page
+
+
+# On the pay page he can see bank details for offline payment
+
+
+# on the pay page he can see a BTC QR Code for instant payment including the amount selected
+
+
+# He chooses bank transfer by doing nothing.
+
+
+# In 3 days he returns and is still logged in.
+
+
+# Since he is logged in, he is taken to the systems/systems page first.
+
+# Alternatvely he is logged in and is taken tot he day trader page
+
+
+# He can see he has credit in one currency.
+
+
+# He chooses the system he liked from before, seeing that the 2016 have been updated.
+
+
+# He subscribes and money is taken out of his account.
+
+
+# After subscribing he sees a confirmation of the subscription period.
+
+
+# After subscribing he can see the extra information on the same systems/detail page id dayalerts
+
+
+# AFter clicking subscribe he can see the new content day alerts right away
 
 
 
