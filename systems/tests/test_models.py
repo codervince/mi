df19b51906c7d67d4cc8da870e246d26169fb947
demonno <demur.nodia@gmail.com>
2016-05-05 09:20:06 +0400

    fix system snapshot custom managers add tests for managers

--- systems/tests/test_models.py
+++ systems/tests/test_models.py
@@ -2,10 +2,12 @@ import unittest
 
 import datetime
 
-from systems.models import System, SystemSnapshot, ss_2016_start
+from django.test import TestCase
+
+from systems.models import System, SystemSnapshot, ss_2016_start, ss_season2016_start, ss_hist_start
 
 
-class TestSnapshotCustomManagers(unittest.TestCase):
+class TestSnapshotCustomManagers(TestCase):
     fixtures = ['investment/fixtures/initial_fund_data.json',
                 'investment/fixtures/initial_system_data.json',
                 'investment/fixtures/user_initial.json']
@@ -15,6 +17,21 @@ class TestSnapshotCustomManagers(unittest.TestCase):
         SystemSnapshot.objects.get_or_create(system=system,
                                              validfrom=ss_2016_start,
                                              validuptonotincluding=datetime.datetime.now())
+        SystemSnapshot.objects.get_or_create(system=system,
+                                             validfrom=ss_season2016_start,
+                                             validuptonotincluding=datetime.datetime.now())
+
+        history_date = ss_hist_start - datetime.timedelta(days=3)
+
+        SystemSnapshot.objects.get_or_create(system=system,
+                                             validuptonotincluding=history_date)
 
     def testSnapshotManagerThisYear(self):
         self.assertEqual(SystemSnapshot.thisyear.all().count(), 1)
+
+    def testSnapshotManagerThisSeason(self):
+        self.assertEqual(SystemSnapshot.thisseason.all().count(), 1)
+
+    def testSnapshotManagerHistorical(self):
+        self.assertEqual(SystemSnapshot.historical.all().count(), 1)
+
